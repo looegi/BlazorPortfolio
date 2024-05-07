6e1fdd99a3b00abcc97edd519de0970a99a568ee
Luis <lc1512@humboldt.edu>
2024-05-06 22:22:59 -0700

    Add project files.

---
+++ LandingPageRazden/LandingPageRazden/Components/Layout/INavItem.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace NavbarInterface.INavItem
+
+
+{
+    public interface INavItem
+
+    {
+        string Name { get; set; }
+        string Link { get; set; }
+        int OrderNum {  get; set; } 
+
+
+
+    }
+}
